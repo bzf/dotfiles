ab4a1faba16d7e6d8fe28917e9e90aaaa080c38f
André Ligné <andre.ligne@gmail.com>
2015-09-12 19:00:03 +0200

    Use rcm

---
+++ tag-xmonad/xmonad/xmonad.hs
@@ -0,0 +1,12 @@
+import XMonad
+
+main = do
+  xmonad $ defaultConfig
+    { terminal    = myTerminal
+      , modMask     = myModMask
+      , borderWidth = myBorderWidth
+    }
+
+myTerminal    = "urxvt"
+myModMask     = mod4Mask -- Win key or Super_L
+myBorderWidth = 3
