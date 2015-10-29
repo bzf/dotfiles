ec00ee60f8b271a993fc9ea848c43fd27dea7199
André Ligné <andre.ligne@gmail.com>
2015-10-29 21:44:11 +0100

    Add xmobar when starting xmonad
    * Add `layout_switch.sh` script for toggling keyboard layout
    * Map `M-w M-w` to execute `layout_switch.sh` script

---
+++ tag-bin/bin/layout_switch.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+# LICENSE: PUBLIC DOMAIN
+# switch between my layouts
+
+# If an explicit layout is provided as an argument, use it. Otherwise, select
+# the next layout from the set [us, se].
+if [[ -n "$1" ]]; then
+  setxkbmap $1 -variant mac
+else
+  layout=$(setxkbmap -query | grep layout | awk 'END{print $2}')
+  case $layout in
+    us)
+      setxkbmap se -variant mac
+      ;;
+    se)
+      setxkbmap us -variant mac
+      ;;
+  esac
+fi
--- tag-xmonad/xmonad/xmonad.hs
+++ tag-xmonad/xmonad/xmonad.hs
@@ -1,11 +1,18 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.EZConfig
 
-main = do
-  xmonad $ defaultConfig
+main = xmonad =<< xmobar myConfig
+
+myConfig = 
+  defaultConfig
     { terminal    = myTerminal
       , modMask     = myModMask
       , borderWidth = myBorderWidth
-    }
+    } `additionalKeysP`
+    [
+      ("M-w M-w", spawn "~/.bin/layout_switch.sh" )
+    ] 
 
 myTerminal    = "urxvt"
 myModMask     = mod4Mask -- Win key or Super_L
