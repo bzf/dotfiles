bbc52374dbdb970017becdac59b90c303465d6db
André Ligné <hello@bzf.se>
2019-12-03 15:27:12 +0100

    Use emacs bindings in zsh
    More convenient to use those keybindings for jumping in lines since then
    you can always use them in other environments where the default seems to
    be using the same (e.g. SSHing into a server that runs `bash`).
    
    Also add the `^R` command for searching the history using `fzf`.

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -1,6 +1,12 @@
-bindkey -v
+bindkey -e
 
-bindkey -M viins '^N' down-line-or-history
-bindkey -M viins '^P' up-line-or-history
-bindkey -M viins '^A' beginning-of-line
-bindkey -M viins '^E' end-of-line
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^X^E" edit-command-line
+
+fzf-history-widget-accept() {
+  fzf-history-widget
+  zle accept-line
+}
+zle     -N     fzf-history-widget-accept
+bindkey '^R' fzf-history-widget-accept
