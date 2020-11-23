bef1d793972014db5e6bf2b56bf7fdc12707eaee
André Ligné <hi@andreligne.se>
2020-11-23 18:39:34 +0100

    Automatically download packages on start-up in Emacs

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,13 +1,25 @@
-(require 'package)
-(package-initialize)
-
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-
-(use-package evil :config (evil-mode))
-
 (setq vc-follow-symlinks nil)
 (setq inhibit-startup-message t)
 
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
 (global-display-line-numbers-mode)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+;; Initialize use-package
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package evil :config (evil-mode))
