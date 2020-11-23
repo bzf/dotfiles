d025ff4a54bc61fa87ce5c679827c773fc927bc1
André Ligné <hi@andreligne.se>
2020-11-23 22:11:43 +0100

    Add ivy and projectile

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -22,4 +22,19 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package evil :config (evil-mode))
+(use-package evil
+  :config (evil-mode))
+
+(use-package ivy
+  :config (ivy-mode 1))
+
+(use-package projectile
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/Projects")
+    (setq projectile-project-search-path '("~/Projects")))
+  (setq projectile-switch-project-action #'projectile-dired)
+  (define-key evil-normal-state-map (kbd "C-p") 'projectile-find-file))
