53e1aeff5c23d2081b8ca24fd1c6a74e154a61bd
André Ligné <hi@andreligne.se>
2020-11-23 18:06:46 +0100

    Hide scroll-bar and tool-bar in Emacs on start-up
    This also hides the startup message since it doesn't really add any value.

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -6,4 +6,8 @@
 (use-package evil :config (evil-mode))
 
 (setq vc-follow-symlinks nil)
+(setq inhibit-startup-message t)
+
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
 (global-display-line-numbers-mode)
