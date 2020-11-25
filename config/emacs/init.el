c9c261abb34f529393064aafb2a98ce07022590e
André Ligné <hi@andreligne.se>
2020-11-25 07:26:47 +0100

    Add colors in terminal when tests in Emacs

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -46,3 +46,13 @@
   :config (global-flycheck-mode))
 
 (use-package magit)
+
+(set-face-attribute 'comint-highlight-prompt nil
+                    :inherit nil)
+
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region (point-min) (point-max))))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
