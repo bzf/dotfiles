7238ab1092293e67431c835e4cbcde72bd1cc148
André Ligné <hi@andreligne.se>
2020-11-30 21:59:23 +0100

    Use the doom-one theme from doom-themes
    https://github.com/hlissner/emacs-doom-themes

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -83,6 +83,15 @@
   :config
   (persp-mode))
 
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+  (load-theme 'doom-one t)
+  (doom-themes-visual-bell-config)
+  (doom-themes-treemacs-config)
+  (doom-themes-org-config))
+
 (set-face-attribute 'comint-highlight-prompt nil
                     :inherit nil)
 
