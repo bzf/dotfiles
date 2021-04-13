f679a3f0535addaa146f60dbbf1e12d0961d4efd
André Ligné <andre@ligne.io>
2021-04-13 08:47:32 +0200

    Install use-package and use for installing evil-mode

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -7,23 +7,9 @@
 (if (not package-archive-contents)
   (package-refresh-contents))
 
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
-(custom-set-variables
-
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package evil
+  :ensure t
+  :config (evil-mode 1))
