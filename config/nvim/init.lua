cf01254e1bf7c5d06a9ff0c50c568b35338ab9a5
André Ligné <andre@ligne.io>
2021-12-09 16:08:56 +0100

    Add packer.nvim and install surround and unimpaired

---
+++ config/nvim/init.lua
@@ -0,0 +1,16 @@
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+
+  use 'tpope/vim-surround'
+  use 'tpope/vim-unimpaired'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
