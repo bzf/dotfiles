d71e95b8a1b07c872c097d8badab758a2fde57fe
André Ligné <andre@ligne.io>
2021-12-09 16:08:57 +0100

    Set up nvim-cmp with lsp support for rust_analyzer

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -94,6 +94,59 @@ require('packer').startup(function()
   }
 
   use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'hrsh7th/cmp-cmdline' },
+      { 'hrsh7th/nvim-cmp' },
+    },
+    config = function()
+      local cmp = require'cmp'
+
+      cmp.setup({
+        mapping = {
+          ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+          ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+          ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+          ['<C-y>'] = cmp.config.disable,
+          ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+          }),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+        }, {
+          { name = 'buffer' },
+        })
+      })
+
+      cmp.setup.cmdline('/', {
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      cmp.setup.cmdline(':', {
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        })
+      })
+
+      local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+      require('lspconfig')['rust_analyzer'].setup {
+        capabilities = capabilities
+      }
+    end
+  }
+
+  use {
     'tpope/vim-fugitive', 
     config = function()
       vim.api.nvim_set_keymap('n', '<leader>gs', ':Git<CR>', { noremap = true, silent = true })
