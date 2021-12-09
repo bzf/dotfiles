46d04925c268a29387772951f2e0da8e728b73e3
André Ligné <andre@ligne.io>
2021-12-09 16:08:57 +0100

    Add vim-fugitive

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -33,6 +33,13 @@ return require('packer').startup(function()
   use 'tpope/vim-unimpaired'
 
   use {
+    'tpope/vim-fugitive', 
+    config = function()
+      vim.api.nvim_set_keymap('n', '<leader>gs', ':Git<CR>', { noremap = true, silent = true })
+    end
+  }
+
+  use {
     'morhetz/gruvbox',
     config = function()
       vim.cmd[[colorscheme gruvbox]]
