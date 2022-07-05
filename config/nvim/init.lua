5f464ffaa33582708a31209c78ab104d63e1f9c4
André Ligné <andre@ligne.io>
2022-07-05 09:14:55 +0200

    Extract ale configuration into its own module

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -11,19 +11,6 @@ vim.opt.shiftwidth = 2
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.g.ale_linters = {ruby = {'standardrb'}, elm = {'elm_ls'}}
-vim.g.ale_fixers = {
-  ['*'] = {'remove_trailing_lines', 'trim_whitespace'},
-  ruby = {'standardrb'},
-  rust = {'rustfmt'},
-  elm = {'elm-format'},
-  typescript = {'prettier'},
-  typescriptreact = {'prettier'}
-}
-
-vim.g.ale_linters_explicit = 1
-vim.g.ale_fix_on_save = 1
-
 vim.api.nvim_set_keymap('n', '<leader>vs', ':source $HOME/.config/nvim/init.lua<CR>', { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', '<leader>w', ':write<CR>', { noremap = true, silent = true })
