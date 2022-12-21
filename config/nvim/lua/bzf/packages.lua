af2699fde9f150da7d735143442f6efc5c984ae1
André Ligné <andre@ligne.io>
2022-12-21 12:21:44 +0100

    Add vim plugin gitsigns

--- config/nvim/lua/bzf/packages.lua
+++ config/nvim/lua/bzf/packages.lua
@@ -117,6 +117,27 @@ M.startup = function()
     }
 
     use {
+      'lewis6991/gitsigns.nvim',
+      config = function()
+        require('gitsigns').setup {
+          on_attach = function(bufnr)
+            local gs = package.loaded.gitsigns
+
+            local function map(mode, l, r, opts)
+              opts = opts or {}
+              opts.buffer = bufnr
+              vim.keymap.set(mode, l, r, opts)
+            end
+
+            map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+            map('n', '<leader>tb', gs.toggle_current_line_blame)
+            map('n', '<leader>td', gs.toggle_deleted)
+          end
+        }
+      end
+    }
+
+    use {
       'tpope/vim-fugitive',
       config = function()
         vim.api.nvim_set_keymap('n', '<leader>gs', ':Git<CR>', { noremap = true, silent = true })
