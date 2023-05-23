dfbb8c66f93edd756fc1ce8d25063de1975d5508
André Ligné <andre@ligne.io>
2023-05-23 09:10:19 +0200

    Ensure some treesitter parsers always are installed

--- config/nvim/lua/bzf/packages.lua
+++ config/nvim/lua/bzf/packages.lua
@@ -32,7 +32,16 @@ M.startup = function()
 
     use {
       'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate'
+      run = ':TSUpdate',
+      config = function()
+        require'nvim-treesitter.configs'.setup {
+          -- A list of parser names, or "all" (the five listed parsers should always be installed)
+          ensure_installed = {"ruby", "javascript", "rust"},
+
+          -- Install parsers synchronously (only applied to `ensure_installed`)
+          -- sync_install = false,
+        }
+      end,
     }
 
     use {
