f4cf4275de6cd068f4ff7a449370deeba9c71603
André Ligné <andre@ligne.io>
2024-05-27 22:26:12 +0200

    Disable mouse and enable relativenumber

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -95,14 +95,11 @@ vim.g.maplocalleader = " "
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = "a"
+vim.opt.mouse = ""
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
