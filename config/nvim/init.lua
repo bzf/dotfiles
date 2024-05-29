3506847d4b177ae6f2f6b2c4eb592e5f89b2185e
André Ligné <andre@teamtailor.com>
2024-05-29 17:14:36 +0200

    Add vim-vinegar and vim-mkdir

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -880,6 +880,10 @@ require("lazy").setup({
 		end,
 	},
 
+	{ "tpope/vim-vinegar" },
+
+	{ "pbrisbin/vim-mkdir" },
+
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
