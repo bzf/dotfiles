4ed2c8132f7a536f58f02314593f5bf0772de00d
André Ligné <andre@teamtailor.com>
2024-05-29 13:55:51 +0200

    Replace 'Search [N]eovim' with 'Search [D]otfiles'

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -356,7 +356,6 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
@@ -379,10 +378,10 @@ require("lazy").setup({
 				})
 			end, { desc = "[S]earch [/] in Open Files" })
 
-			-- Shortcut for searching your Neovim configuration files
-			vim.keymap.set("n", "<leader>sn", function()
-				builtin.find_files({ cwd = vim.fn.stdpath("config") })
-			end, { desc = "[S]earch [N]eovim files" })
+			-- Shortcut for searching your dotfiles configuration files
+			vim.keymap.set("n", "<leader>sd", function()
+				builtin.find_files({ cwd = "~/.dotfiles" })
+			end, { desc = "[S]earch [D]otfiles" })
 		end,
 	},
 
