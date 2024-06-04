9ec25a7009c5a98196909682aa543265e798195c
André Ligné <andre@teamtailor.com>
2024-06-04 12:00:14 +0200

    Set config.dev.path to ~/dev

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -935,6 +935,9 @@ require("lazy").setup({
 	--    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
 	-- { import = 'custom.plugins' },
 }, {
+	dev = {
+		path = "~/dev",
+	},
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
 		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
