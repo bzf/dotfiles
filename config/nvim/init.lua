2bf509647642cc909a0bf9fb0911f892ccb61bd2
André Ligné <andre@teamtailor.com>
2024-08-19 11:46:56 +0200

    Increase the terminal scrollback to 100 000 lines
    Useful when running commands that output lots of stuff (e.g. when
    running profilers/tests) so it's nice to be to scroll around it all.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -145,6 +145,7 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+vim.opt.scrollback = 100000
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
